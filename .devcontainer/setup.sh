25c33f21ec314c2cd5ffd2eeb979d47f614a8798
Alan K <afksavish@gmail.com>
2022-12-23 18:11:54 +0200

    Remove initial shell exec

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -31,5 +31,3 @@ cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
-
-exec zsh
